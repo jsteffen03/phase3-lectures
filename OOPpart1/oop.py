896159c1bedceaf991c330ccb2622aeb45d8c039
David Doan <daviddoan@Davids-MacBook-Pro.local>
2023-03-15 15:57:16 -0600

    Solution

--- OOPpart1/oop.py
+++ OOPpart1/oop.py
@@ -1,3 +1,20 @@
 # What is object oriented programming?
+x = 5
+bird = 5
+bird = "Geese" 
 # How do we expand it? What else does it contain? What else could it contain?
+bird.flight = True
+bird.size = "intimidating"
+class bird:
+    name
+    species
+    flight
+    size
+    wingspan
+    flight = []
+    def newposition(position):
+        flight.append(position)
+    
+geese = bird()
+geese.newposition(12)
 # What else do we want to do with this? Thinking about oop what functions can we create
--- OOPpart1/review.py
+++ OOPpart1/review.py
@@ -1,10 +1,34 @@
 # List
-# Dict
+x = [1,2,3,3,3]
+# Dict left is key, right is value
+obj = {
+    "A": 1,
+    "B": 3,
+    "C": 6,
+    "C": 6
+}
 # Set
+objset = set(obj)
+xset = set(x)
+print(xset,objset)
+newset = {1,2,3}
 
 # Things I missed/points brought up
 # Using range()
+for i in range(len(x)):
+    print(i)
+
 # Using type()
+if type(x) is list:
+    print("Is list")
+
 # Comparing type, using and,is, or
+if 2 in x or 5 in x:
+    print("Is in")
+
 # Listifying strings
+test = list("Type")
+print(test)
 # Raise an error (raise https://docs.python.org/3/library/exceptions.html)
+if 2 in x or 5 in x:
+    raise TypeError("Not in")
