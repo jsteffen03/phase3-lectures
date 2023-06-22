ae7768099fca7322e5f74b22ef2a6cca1c9f4053
David Doan <daviddoan@Davids-MacBook-Pro.local>
2023-06-22 14:04:40 -0600

    Changed wording

--- 6_sqlAlchemy1/sqlalchemy_test.py
+++ 6_sqlAlchemy1/sqlalchemy_test.py
@@ -4,56 +4,27 @@ from sqlalchemy import Column, Integer, String, create_engine, func
 from sqlalchemy.orm import Session, declarative_base
 
 # We start with setting up a declrative_base()
-Base = declarative_base()
 # Now we create a class that is a child of that Base
-class Trails(Base):
-    __tablename__ = 'trails'
-    id = Column(Integer(), primary_key=True)
-    name = Column(String())
-class Mountains(Base):
+class Class1(Base):
+    pass
+class Class2(Base):
     #Set up a __tablename__ as the tablename!
-    __tablename__ = 'mountains'
 
     '''
     Now (Without init) we can create the column 
     of the table (don't forget the primary_key=true) 
     using our Column import Column(Datatype())
     '''
-    id = Column(Integer(), primary_key=True)
-    name = Column(String())
-    elevation = Column(Integer())
-    cr = Column(Integer())
-    location = Column(String())
-    trailnumbers = Column(Integer())
-    nickname = Column(String())
-    # This will make it easier to read
-    def __repr__(self):
-        return f"{self.name}: Location:{self.location}"
-    pass
 
 if __name__ == '__main__':
     #We can go ahead and create the db engine (creates the file as well)
-    engine = create_engine('sqlite:///mountains.db')
+    engine = create_engine('sqlite:///something.db')
 
     # create schema (which will create the mountain schema)
     Base.metadata.create_all(engine)
     
     # Now we can use an with statement
-    with Session(engine) as session:
-        alldata = session.query(Mountains
-        ).delete()
-        
-        # Add
-        newmountain = Mountains(
-            name ="Mt.Everest",
-            elevation = 200000,
-            cr = 9001,
-            location = "Nepal",
-            trailnumbers = 4
-            )
-        # print(newmountain)
-        session.add(newmountain)
-        session.commit()
+    # with Session(engine) as session:
     # Within this with we now do anything with the session! 
     '''
     Session.add()
